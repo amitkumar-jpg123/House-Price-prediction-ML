65d85725ea1cb8e49f93d35a75add675527597cc
Amit Kumar <amitkumara59744@gmail.com>
2025-12-28 15:14:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,22 @@ import time
 from sklearn.datasets import fetch_california_housing
 
 st.title('🏠House Price prediction using ML')
-
 st.image('https://blog.architizer.com/wp-content/uploads/Untitled-design.gif')
+
+df = pd.read_csv('house_data.csv')
+X =  df.iloc[:,:-3]
+y = df.iloc[:,:-1]
+
+st.sidebar.title(' Select House features')
+st.sidebar.image('https://blog.architizer.com/wp-content/uploads/Untitled-design.gif')
+all_value = []
+for i in X:
+  ans = st.sidebar.slider(f'Select {i} value')
+  all_value.append(ans)
+
+st.write(all_value)
+  
+
 
 
 
