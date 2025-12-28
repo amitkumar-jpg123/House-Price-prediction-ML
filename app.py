a950778dfa9c75fb3d74a21acf5857aa7074fc37
Amit Kumar <amitkumara59744@gmail.com>
2025-12-28 15:38:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,11 +16,15 @@ st.sidebar.title(' Select House features')
 st.sidebar.image('https://blog.architizer.com/wp-content/uploads/Untitled-design.gif')
 all_value = []
 for i in X:
-  ans = st.sidebar.slider(f'Select {i} value')
+  min_value = int(X[i].min())
+  max_value = int(X[i].max())
+  ans = st.sidebar.slider(f'Select {i} value',min_value,max_value)
   all_value.append(ans)
 
-st.write(all_value)
+# st.write(all_value)
+
   
+
 
 
 
