8c652d936199bdc8c50f8a9bcfea9b36ee726c67
Amit Kumar <amitkumara59744@gmail.com>
2025-12-28 16:29:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ scaled_X = scaler.fit_transform(X)
 final_value = scaler.transform([all_value])
 
 model = st.cache(RandomForestRegressor)()
-model.fit(X,y)
+st.cache(model.fit(X,y))
 house_price = model.predict(final_value)[0][0]
 
 with st.spinner('Predicting House price'):
@@ -42,6 +42,7 @@ st.success(msg)
 st.markdown('''**Design and Developed by: Amit Kumar''')
 
   
+
 
 
 
